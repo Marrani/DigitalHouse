f5f02f3937b80c16bc4b9727317449cedb7e8d09
Marrani <gabrielmarrani@gmail.com>
2020-09-10 12:15:07 -0300

    Atividades propostas na aula

---
+++ Aula04/src/Nivel01/at03.kt
@@ -0,0 +1,9 @@
+// 3. Faça um Programa que converta metros para centímetros.
+
+fun main(){
+
+    var metros = readLine()!!.toInt();
+    var centimetros = metros * 100;
+    print("$metros Metros é igual $centimetros centimetros");
+
+0}
