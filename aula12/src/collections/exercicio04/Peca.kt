499972c0de3b51ed192d4fd4ebdd188792975199
Marrani <gabrielmarrani@gmail.com>
2020-09-26 17:37:18 -0300

    Exercicios

--- aula12/src/collections/exercicio04/Peca.kt
+++ aula12/src/collections/exercicio04/Peca.kt
@@ -1,9 +1,10 @@
 package collections.exercicio04
 
-abstract class peca(var marca:String, var modelo:String) {
+abstract class  Peca(var marca:String, var modelo:String) {
 
-    fun retirada(){
-
-
+    abstract fun retirada()
+    override fun toString(): String {
+        return "Peca(marca='$marca', modelo='$modelo')"
     }
+
 }
